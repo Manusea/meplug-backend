a9f490cbb3e34ae9d2b314938706ba6ffd1f907f
Mansea <6210742034@student.tu.ac.th>
2022-07-20 14:11:57 +0700

    Authentication

--- routes/Authentication.js
+++ routes/Authentication.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var controller = require('../controllers/AuthController');
 
 router.post('/register', controller.register)
+router.post('/login', controller.login)
 
 
 module.exports = router;
