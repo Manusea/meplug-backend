32cafd2e3cd1f44221008142b42e1a12cda55c18
Mansea <6210742034@student.tu.ac.th>
2022-07-20 14:11:57 +0700

    redux implementation

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const express = require('express')
-const bodyParser = require('body-parser')
 const dotenv = require('dotenv');
 
 const app = express()
@@ -19,15 +18,12 @@ db.once('open',()=>{
     console.log('Connected to MongoDB....');
 })
 
-app.post('/welcome', (req, res) =>{
-    res.status(200).send("Welcome")
-})
+app.use(express.json())
+
 
 app.use('/auths', AuthRoutes);
 
 
-
-app.use(bodyParser.json());
 
 
 app.listen(port,console.log("Listening on port: 127.0.0.1:",port))
--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,8 +1,59 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs/dist/bcrypt');
+const User = require("../models/Users");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const {
+  registerValidation,
+  loginValidation,
+} = require("../components/validation");
 
 const register = async (req, res) => {
-    res.status(200).send("Register Test")
-}
+  //Validate data before add it
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-module.exports = {register}
+  //Check if user is already in database
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exists!!");
+
+  //Hash passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashPassword,
+  });
+
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+const login = async (req, res) => {
+  //Validate data before add it
+  const {error} = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //Check if email exists
+  const user = await User.findOne({email: req.body.email}).select('+password');
+  if (!user) return res.status(400).send('Email does not exists!!');
+
+  //Password is correct
+  const validPass = await bcrypt.compare(req.body.password, user.password)
+  if (!validPass) return res.status(400).send('Invalid password!!')
+
+  var user_data = await User.find({_id: user.id}, {_id: 0,date: 0,__v: 0});
+
+  //Create and assign a token
+  const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+  res.header('auth-token', token).send({success: true, token: token, data: user_data});
+};
+
+const logout = async (req, res) => {
+    res.send({success: true, message: "Logout"})
+}
+module.exports = { register, login, logout };
